9edc837feac4e7a5d200c1e3a5bd83e1f7cebc65
Michs03 <michellerodriguezm016@gmail.com>
2025-07-29 17:50:23 -0500

    Cambios
    Cambios ultimos

--- Js/componente.js
+++ Js/componente.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+    fetch("header.html")
+      .then(res => res.ok ? res.text() : Promise.reject("Error al cargar header"))
+      .then(html => {
+        document.getElementById("header").innerHTML = html;
+      })
+      .catch(err => console.error(err));
+
+    fetch("footer.html")
+      .then(res => res.ok ? res.text() : Promise.reject("Error al cargar footer"))
+      .then(html => {
+        document.getElementById("footer").innerHTML = html;
+      })
+      .catch(err => console.error(err));
+
     setTimeout(() => {
       const slides = document.querySelectorAll(".slide");
       const prevBtn = document.querySelector(".prev");
